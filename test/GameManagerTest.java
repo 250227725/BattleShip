6aa730d5c41e5b10be092ea3f702d08f8930baa5
Sergei Azarov <sergeiazarov@gmail.com>
2021-10-11 07:16:35 +0300

    Modify test case fo GameManager.class

--- test/GameManagerTest.java
+++ test/GameManagerTest.java
@@ -24,36 +24,28 @@ public class GameManagerTest {
 
     @Test
     public void one_cycle_of_nextPlayer_is_fully_ordered_players_list() {
-        GameManager manager = new GameManager();
-        Set<Player> initPlayers = new HashSet<>(manager.getPlayers());
-        initPlayers.add(new Player());
-        initPlayers.add(new Player());
-        initPlayers.add(new Player());
-        for (Player player : initPlayers) {
-            manager.addPlayer(player);
+        List<Player> initPlayers = new ArrayList<>();
+        int playerQuantity = (int) (Math.random() * 10 + 3);
+        for (int i = 0; i < playerQuantity; i++) {
+            initPlayers.add(new Player());
         }
-
-        manager.createPlayerSequence();
+        GameManager manager = new GameManager(initPlayers);
 
         Set<Player> orderedPlayers = new HashSet<>();
         for (int i = 0; i < initPlayers.size(); i++) {
             orderedPlayers.add(manager.getNextPlayer());
         }
-        assertThat(initPlayers, equalTo(orderedPlayers));
+        assertThat(new HashSet<Player>(initPlayers), equalTo(orderedPlayers));
     }
 
     @Test
     public void sequenced_nextPlayer_for_whole_game_cycle() {
-        GameManager manager = new GameManager();
-        Set<Player> initPlayers = new HashSet<>(manager.getPlayers());
-        initPlayers.add(new Player());
-        initPlayers.add(new Player());
-        initPlayers.add(new Player());
-        for (Player player : initPlayers) {
-            manager.addPlayer(player);
+        List<Player> initPlayers = new ArrayList<>();
+        int playerQuantity = (int) (Math.random() * 10 + 3);
+        for (int i = 0; i < playerQuantity; i++) {
+            initPlayers.add(new Player());
         }
-
-        manager.createPlayerSequence();
+        GameManager manager = new GameManager(initPlayers);
 
         List<Player> orderedPlayers = new ArrayList<>();
         for (int i = 0; i < initPlayers.size(); i++) {
@@ -74,16 +66,12 @@ public class GameManagerTest {
     doesn't mean what method is incorrect. You can run it several times to check result
      */
     public void player_sequence_is_differed_from_init_player_list() {
-        GameManager manager = new GameManager();
-        List<Player> initPlayers = new ArrayList<>(manager.getPlayers());
-        initPlayers.add(new Player());
-        initPlayers.add(new Player());
-        initPlayers.add(new Player());
-        for (Player player : initPlayers) {
-            manager.addPlayer(player);
+        List<Player> initPlayers = new ArrayList<>();
+        int playerQuantity = (int) (Math.random() * 10 + 3);
+        for (int i = 0; i < playerQuantity; i++) {
+            initPlayers.add(new Player());
         }
-
-        manager.createPlayerSequence();
+        GameManager manager = new GameManager(initPlayers);
 
         List<Player> orderedPlayers = new ArrayList<>();
         for (int i = 0; i < initPlayers.size(); i++) {

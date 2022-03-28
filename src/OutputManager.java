72694684c1ac636f780c2a36f9b3491d470b64e7
Sergei Azarov <sergeiazarov@gmail.com>
2022-03-28 11:18:59 +0300

    set OutputManager for Intreface and create ConsoleOutputManager

--- src/OutputManager.java
+++ src/OutputManager.java
@@ -1,17 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class OutputManager {
-    private static final OutputManager instance = new OutputManager();
-
-    private OutputManager() {
-    }
-
-    public OutputManager getInstance() {
-        return instance;
-    }
-
-    public void showMessage(String message) {
-        System.out.println(message);
-    }
+public interface OutputManager {
+    void showMessage(String message);
 }

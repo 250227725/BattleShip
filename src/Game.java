cc783a9bd6f33ca4d489a2c2385980192250149e
Sergei Azarov <sergeiazarov@gmail.com>
2022-03-30 19:15:32 +0300

    Added base functionality for Game class

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,31 @@
 import java.util.Deque;
-import java.util.LinkedList;
 
-public class Game {
-    private final Deque<Player> players = new LinkedList<>();
+public class Game implements Runnable{
+    private final Deque<Player> players;
+    private GameStatus status = GameStatus.NEW;
+    private Game(Deque<Player> players) {
+        this.players = players;
+    }
+
+    public static Game createGame(Deque<Player> players) throws IllegalArgumentException {
+        return new Game(players);
+    }
+
+    public boolean isActive() {
+        return status == GameStatus.ACTIVE;
+    }
+
+    public boolean isEnded() {
+        return status == GameStatus.ENDED;
+    }
+
+    public void run() {
+        status = GameStatus.ACTIVE;
+    }
+
+    enum GameStatus {
+        NEW,
+        ACTIVE,
+        ENDED
+    }
 }

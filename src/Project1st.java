9afa23c984d6efea079324b2a29686cfb91eac3b
Sergei Azarov <sergeiazarov@gmail.com>
2022-03-31 17:24:52 +0300

    temproary added filds height and width for the Base class

--- src/Project1st.java
+++ src/Project1st.java
@@ -3,6 +3,10 @@ public class Project1st {
     public static final int MAX_FIELD_WIDTH = CellStatus.values().length;
     public static final int MAX_FIELD_HEIGHT = 100;
 
+    //todo: Delete next two poarametrs and modify method for using Game class instance fields
+    public static final int FIELD_WIDTH = 10;
+    public static final int FIELD_HEIGHT = 10;
+
     public static void main(String... args) {
         //todo:
         /*

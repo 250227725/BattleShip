cc3709b93ea792681fbd9d87db064cd1aa980c4c
Sergei Azarov <sergeiazarov@gmail.com>
2022-04-02 17:55:59 +0300

    Create static filed GameService in base class

--- src/Project1st.java
+++ src/Project1st.java
@@ -5,13 +5,14 @@ public class Project1st {
     public static final int MIN_FIELD_WIDTH = 5;
     public static final int MIN_FIELD_HEIGHT = 5;
     public static final int[] shipsSetup = new int[]{0,4,3,2,1}; //todo: It should depend on gamefield size;
+    public static final GameService service = GameService.getInstance(new IOManager(ConsoleInputManager.getInstance(), ConsoleOutputManager.getInstance()));
 
     //todo: Delete next two poarametrs and modify method for using Game class instance fields
     public static final int FIELD_WIDTH = 10;
     public static final int FIELD_HEIGHT = 10;
 
     public static void main(String... args) {
-        GameService service = GameService.getInstance(new IOManager(ConsoleInputManager.getInstance(), ConsoleOutputManager.getInstance()));
+
         try {
             Game game = service.initGame();
             game.call();

b832c4ab3a827444200bb21cc4679d07112bcbd6
Sergei Azarov <sergeiazarov@gmail.com>
2022-04-02 13:26:02 +0300

    add isHuman to invokation Player contructor

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -9,20 +9,20 @@ public class PlayerTest {
     @Test
     public void saveCorrectedNameTest() {
         String name = "John Smith";
-        Player player = new Player(name);
+        Player player = new Player(name, true);
         assertThat(player.getName(), equalTo(name));
     }
 
     @Test
     public void addShipTest() {
-        Player player = new Player("Test");
+        Player player = new Player("Test", true);
         player.addShip(new int[][]{{0,0}, {0,1}});
         assertThat(player.getShips().size(), equalTo(1));
     }
 
     @Test
     public void multiAddTest() {
-        Player player = new Player("Test");
+        Player player = new Player("Test", true);
         int lenght = ((int) (Math.random() * 5 + 1));
         for (int i = 0; i < lenght; i++) {
             player.addShip(new int[][]{{i * 2, i * 2}});
@@ -32,21 +32,21 @@ public class PlayerTest {
 
     @Test
     public void dublicateAddTest() throws IllegalArgumentException{
-        Player player = new Player("Test");
+        Player player = new Player("Test", true);
         player.addShip(new int[][]{{0,0}, {0,1}});
         assertThrows(IllegalArgumentException.class, () -> player.addShip(new int[][]{{0,0}, {0,1}}));
     }
 
     @Test
     public void neighbourAddHorizontalTest() throws IllegalArgumentException{
-        Player player = new Player("Test");
+        Player player = new Player("Test", true);
         player.addShip(new int[][]{{0,0}, {0,1}});
         assertThrows(IllegalArgumentException.class, () -> player.addShip(new int[][]{{0,2}, {0,3}}));
     }
 
     @Test
     public void neighbourAddVerticalTest() throws IllegalArgumentException{
-        Player player = new Player("Test");
+        Player player = new Player("Test", true);
         player.addShip(new int[][]{{0,0}, {0,1}});
         assertThrows(IllegalArgumentException.class, () -> player.addShip(new int[][]{{1,0}, {1,1}}));
     }

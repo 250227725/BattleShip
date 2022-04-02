7a790de8bf841e8c4d63a14818dde3bfc4cd09a0
Sergei Azarov <sergeiazarov@gmail.com>
2022-04-02 12:41:19 +0300

    add min_field dimension constants

--- src/Project1st.java
+++ src/Project1st.java
@@ -2,6 +2,8 @@ public class Project1st {
 
     public static final int MAX_FIELD_WIDTH = Cell.HorizontalCellNames.values().length;
     public static final int MAX_FIELD_HEIGHT = 100;
+    public static final int MIN_FIELD_WIDTH = 5;
+    public static final int MIN_FIELD_HEIGHT = 5;
 
     //todo: Delete next two poarametrs and modify method for using Game class instance fields
     public static final int FIELD_WIDTH = 10;
